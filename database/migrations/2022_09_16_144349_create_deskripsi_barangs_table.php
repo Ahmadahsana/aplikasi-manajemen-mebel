1fa99ef758914dfd37cba9f2a307afd02bbdb33a
Ahmadahsana <ahmadahsanataqwim@gmail.com>
2022-11-30 19:42:47 +0700

    first commit

---
+++ database/migrations/2022_09_16_144349_create_deskripsi_barangs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('deskripsi_barangs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('barang_id');
+            $table->string('deskripsi');
+            $table->string('harga');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('deskripsi_barangs');
+    }
+};
